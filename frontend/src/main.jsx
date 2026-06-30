7d5fbcffe0959b564d83a6ed0392b85d7bd6d9c0
Khawaja Syed Hamdan Ahmed <hamdanahmed.2004.16@gmail.com>
2026-06-30 16:29:19 +0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import "../../styles/globals.css";
+import "./globals.css";
 import App from "./App.jsx";
 
 const container = document.getElementById("root");
